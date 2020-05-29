ceabef569775b4e5334dd3d1074d6df6cd82bc4d
thaksin <thaksin@aspiron.co.th>
2020-05-29 15:57:25 +0700

    fixed path file

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,11 +9,11 @@ app.get('/api/getUser', (req, res) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'))
+  app.use(express.static('../../frontend/build'))
 
   const path = require('path')
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'))
   })
 }
 
